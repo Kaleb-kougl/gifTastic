4793a2ca04c161a2e2a9dd471c3092a733708229
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-20 16:21:52 -0600

    handles adding images

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-  // Storing our giphy API URL for a random cat image
-  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
-
 $(document).on("click", ".btn", function() {
   // Grabbing and storing the data-animal property value from the button
   let food = $(this).attr("data-food");
   console.log(food);
 
   // Constructing a queryURL using the animal name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     food + "&api_key=dc6zaTOxFJmzC&limit=10";
 
   // Performing an AJAX request with the queryURL
@@ -18,11 +15,11 @@ $(document).on("click", ".btn", function() {
     // After data comes back from the request
   .then(function(response) {
     $("#images").empty();
-    var results = response.data;
-    for (var i = 0; i < results.length; i++) {
-      var foodDiv = $("<div>");
-      var p = $("<p>").text("Rating: " + results[i].rating);
-      var animalImage = $("<img>");
+    let results = response.data;
+    for (let i = 0; i < results.length; i++) {
+      let foodDiv = $("<div>");
+      let p = $("<p>").text("Rating: " + results[i].rating);
+      let animalImage = $("<img>");
       animalImage.attr("src", results[i].images.fixed_height.url);
       foodDiv.append(p);
       foodDiv.append(animalImage);
