09a90ea63c0f01fecc4c07175ee108380d8c1a30
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-20 16:42:10 -0600

    adding data-animate, data-still, adding toggle listener between animated and still

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let topics =['beef', 'chicken', 'ramen'];
+
+  for (let i = 0; i < topics.length; i++) {
+    console.log('here')
+    let newBtn = $(`<button class="btn" data-food=${topics[i]}>`);
+    newBtn.text(topics[i]);
+    $('#button-div').append(newBtn);
+}
+
 $(document).on("click", ".btn", function() {
   // Grabbing and storing the data-animal property value from the button
   let food = $(this).attr("data-food");
@@ -17,10 +26,16 @@ $(document).on("click", ".btn", function() {
     $("#images").empty();
     let results = response.data;
     for (let i = 0; i < results.length; i++) {
+      console.log(results[i].images);
       let foodDiv = $("<div>");
       let p = $("<p>").text("Rating: " + results[i].rating);
       let animalImage = $("<img>");
-      animalImage.attr("src", results[i].images.fixed_height.url);
+      // animalImage.attr("src", results[i].images.fixed_height.url);
+      animalImage.attr("src", results[i].images.fixed_height_still.url)
+      animalImage.data('state', 'still')
+      animalImage.attr('data-animate', results[i].images.fixed_height.url)
+      animalImage.attr('data-still', results[i].images.fixed_height_still.url)
+      animalImage.attr('class', 'gif')
       foodDiv.append(p);
       foodDiv.append(animalImage);
       $("#images").prepend(foodDiv);
@@ -36,3 +51,14 @@ $('#submission-form').on('submit', function(event) {
   newBtn.text(food);
   $('#button-div').append(newBtn);
 })
+
+$(document).on("click", ".gif", function() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
